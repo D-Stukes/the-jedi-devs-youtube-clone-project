d4a23582beb3ca0f1b9c28feb0158353eda049f3
Lennie Nurse <lnursej@gmail.com>
2022-11-17 11:29:57 -0500

    "update"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from "react"
-import Home from "./Components/Home";
+import Home from "./Components/Home"
 import About from "./Components/About"
 import OneVideo from "./Components/OneVideo"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-
 
 function App() {
   return (
     <div className="main-grid-container">
       <Router>
-     {/* <h1>The Jedi Devs YouTube Clone Project</h1> */}
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/selectedVideo" element={<OneVideo />} />
-      </Routes>
-      
-     </Router>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/selectedVideo" element={<OneVideo />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
