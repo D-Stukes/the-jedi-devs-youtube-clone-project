7d6825e8c02df452a52aa683cd1d3df81e83d0b2
Lennie Nurse <lnursej@gmail.com>
2022-11-22 15:25:12 -0500

    "fetch component"

---
+++ src/api/fetch.js
@@ -0,0 +1,9 @@
+const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=&key=${process.env.REACT_APP_API_KEY}`
+
+export function getAllVideos() {
+  return fetch(`${BASE_URL}`).then((res) => res.json())
+}
+
+export function getOneVideo(id) {
+  return fetch(`BASE_URL/oneVideo/${id}`).then((res) => res.json())
+}
